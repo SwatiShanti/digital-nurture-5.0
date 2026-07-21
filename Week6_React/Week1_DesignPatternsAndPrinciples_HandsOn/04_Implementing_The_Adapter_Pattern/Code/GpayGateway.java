6a952dbba23948476b6118c64da3f00aa98b0993
{swaTI} <{swati}>
2026-07-21 18:02:24 +0530

    remove extra folders from week 6

--- Week6_React/Week1_DesignPatternsAndPrinciples_HandsOn/04_Implementing_The_Adapter_Pattern/Code/GpayGateway.java
+++
@@ -1,7 +0,0 @@
-public class GpayGateway {
-    public void pay(double amount){
-        System.out.println("G-Pay Gateway");
-        System.out.println("Amount : "+amount);
-        System.out.println("Payment Successful");
-    }
-}
--- Week6_React/Week1_DesignPatternsAndPrinciples_HandsOn/04_Implementing_The_Adapter_Pattern/Code/PaypalGateway.java
+++
@@ -1,7 +0,0 @@
-public class PaypalGateway {
-    public void pay(double amount){
-        System.out.println("Paypal Gateway");
-        System.out.println("Amount :"+amount);
-        System.out.println("Payment Successful");
-    }
-}
