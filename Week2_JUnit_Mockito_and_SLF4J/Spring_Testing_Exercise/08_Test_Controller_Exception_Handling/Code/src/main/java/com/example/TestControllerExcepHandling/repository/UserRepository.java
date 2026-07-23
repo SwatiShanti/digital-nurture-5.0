ff16f4536c09067aeb0d69934e6f3b535c5c1f6b
{swaTI} <{swati}>
2026-07-23 11:41:41 +0530

    Reorganize folders for Week 1 (Part 287)

--- Week2_JUnit_Mockito_and_SLF4J/Spring_Testing_Exercise/08_Test_Controller_Exception_Handling/Code/src/main/java/com/example/TestControllerExcepHandling/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.TestControllerExcepHandling.repository;
-
-import com.example.TestControllerExcepHandling.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
