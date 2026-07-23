44ce33962174491a1da12f74cf79226a253de092
{swaTI} <{swati}>
2026-07-23 11:41:37 +0530

    Reorganize folders for Week 1 (Part 407)

--- Week2_JUnit_Mockito_and_SLF4J/Spring_Testing_Exercise/06_Test_Service_Exception_Handling/Code/src/main/java/com/example/TestExceptionHandling/DemoApplication.java
+++
@@ -1,4 +0,0 @@
-package com.example.TestExceptionHandling;
-
-public class DemoApplication {
-}
--- Week2_JUnit_Mockito_and_SLF4J/Spring_Testing_Exercise/06_Test_Service_Exception_Handling/Code/src/main/java/com/example/TestExceptionHandling/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.TestExceptionHandling.repository;
-
-import com.example.TestExceptionHandling.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
