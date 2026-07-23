29b39cbb4dfc8b39e1135f291208b328bf5e1f1e
{swaTI} <{swati}>
2026-07-23 11:53:54 +0530

    Reorganize folders for Week 1 (Part 876)

--- Week3_Spring_Data_JPA_with_Hibernate/02_spring_data_jpa_handson/06_Implement_many_to_many_relationship_between_Employee_and_Skill/Code/orm-learn/src/main/java/com/cognizant/ormlearn/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.ormlearn.repository;
-
-import com.cognizant.ormlearn.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
