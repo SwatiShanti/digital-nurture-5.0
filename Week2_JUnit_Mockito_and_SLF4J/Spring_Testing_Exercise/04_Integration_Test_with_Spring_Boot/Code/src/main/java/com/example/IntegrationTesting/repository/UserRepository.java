34639713fa1802a3ce0f7de2a2b030670e5788f2
{swaTI} <{swati}>
2026-07-23 11:41:33 +0530

    Reorganize folders for Week 1 (Part 670)

--- Week2_JUnit_Mockito_and_SLF4J/Spring_Testing_Exercise/04_Integration_Test_with_Spring_Boot/Code/src/main/java/com/example/IntegrationTesting/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.IntegrationTesting.repository;
-
-import com.example.IntegrationTesting.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
--- Week2_JUnit_Mockito_and_SLF4J/Spring_Testing_Exercise/04_Integration_Test_with_Spring_Boot/Code/src/main/java/com/example/IntegrationTesting/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.IntegrationTesting.service;
-
-import com.example.IntegrationTesting.entity.User;
-
-public interface UserService {
-    User getUserById(Long id);
-}
